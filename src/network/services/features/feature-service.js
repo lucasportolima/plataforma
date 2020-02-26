38d1519e91ea28b1f8bc3dfaaf87e08695448cc0
Lucas Porto <lucas.porto@aisdigital.com.br>
2020-02-26 16:21:39 -0300

    remove codes

--- src/network/services/features/feature-service.js
+++
@@ -1,9 +0,0 @@
-import BaseService from '@/network/base-service'
-
-class FeatureService extends BaseService {
-  constructor () {
-    super(`${process.env.VUE_APP_API_PARTNER_CUSTOMIZATION}/features`)
-  }
-}
-
-export default new FeatureService()
--- src/network/services/login/login-service.js
+++
@@ -1,9 +0,0 @@
-import BaseService from '@/network/base-service'
-
-class LoginService extends BaseService {
-  constructor () {
-    super(`${process.env.VUE_APP_API_AUTH}/auth/login`)
-  }
-}
-
-export default new LoginService()
